6bc2fee3257e31a6eb64a168e3b0b8934381eed3
Christopher Sanchez <diegsanchez27@gmail.com>
2025-10-01 13:57:55 -0600

    Projecto iniciado

---
+++ src/main/java/RECU_POO/ChristopherSanchez20230062/ChristopherSanchez20230062Application.java
@@ -0,0 +1,13 @@
+package RECU_POO.ChristopherSanchez20230062;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristopherSanchez20230062Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristopherSanchez20230062Application.class, args);
+	}
+
+}
---
+++ src/test/java/RECU_POO/ChristopherSanchez20230062/ChristopherSanchez20230062ApplicationTests.java
@@ -0,0 +1,13 @@
+package RECU_POO.ChristopherSanchez20230062;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristopherSanchez20230062ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
